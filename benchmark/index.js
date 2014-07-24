83727b9709f16965441e894d4cfa173526ec09bf
wooorm <tituswormer@gmail.com>
2014-07-24 15:50:55 +0200

    Refactored benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,82 +1,53 @@
 'use strict';
 
-var Retext, source, tiny, small, medium, large;
+var Retext, sentence, paragraph, section, article;
 
 Retext = require('..');
 
-source = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ' +
-    'ac ultricies diam, quis vehicula mauris. Vivamus accumsan eleifend ' +
-    'quam et varius. Etiam congue id magna eu fermentum. Aliquam mollis ' +
-    'adipiscing.\n\n';
-
 /* Test data */
-tiny = source;
-small = Array(11).join(source);
-medium = Array(11).join(small);
-large = Array(11).join(medium);
+
+/* Source: http://www.gutenberg.org/files/10745/10745-h/10745-h.htm */
+
+/* A sentence, 20 words. */
+sentence = 'Where she had stood was clear, and she was gone since Sir ' +
+    'Kay does not choose to assume my quarrel.';
+
+/* A paragraph, 5 sentences, 100 words. */
+paragraph = 'Thou art a churlish knight to so affront a lady ' +
+    'he could not sit upon his horse any longer. ' +
+    'For methinks something hath befallen my lord and that he ' +
+    'then, after a while, he cried out in great voice. ' +
+    'For that light in the sky lieth in the south ' +
+    'then Queen Helen fell down in a swoon, and lay. ' +
+    'Touch me not, for I am not mortal, but Fay ' +
+    'so the Lady of the Lake vanished away, everything behind. ' +
+    sentence;
+
+/* A section, 10 paragraphs, 50 sentences, 1,000 words. */
+section = paragraph + Array(10).join('\n\n' + paragraph);
+
+/* An article, 100 paragraphs, 500 sentences, 10,000 words. */
+article = section + Array(10).join('\n\n' + section);
 
 /* Benchmarks */
-suite('retext.parse(source); // Reuse instance', function () {
-    set('iterations', 20);
-
+suite('retext.parse(source);', function () {
     var retext = new Retext();
-    bench('tiny (1 paragraph, 5 sentences, 30 words, 208 B)',
-        function (done) {
-            retext.parse(tiny);
-            done();
+
+    set('mintime', 100);
+
+    bench('A paragraph (5 sentences, 100 words)', function () {
+        retext.parse(paragraph);
+    });
+
+    bench('A section (10 paragraphs, 50 sentences, 1,000 words)',
+        function () {
+            retext.parse(section);
         }
     );
 
-    bench('small (10 paragraphs, 50 sentences, 300 words, 2 kB)',
-        function (done) {
-            retext.parse(small);
-            done();
-        }
-    );
-
-    bench('medium (100 paragraphs, 500 sentences, 3000 words, 21 kB)',
-        function (done) {
-            retext.parse(medium);
-            done();
-        }
-    );
-
-    bench('large (1000 paragraphs, 5000 sentences, 30000 words, 208 kB)',
-        function (done) {
-            retext.parse(large);
-            done();
-        }
-    );
-});
-
-suite('new Retext().parse(source); // Create new instance', function () {
-    set('iterations', 20);
-
-    bench('tiny (1 paragraph, 5 sentences, 30 words, 208 B)',
-        function (done) {
-            new Retext().parse(tiny);
-            done();
-        }
-    );
-
-    bench('small (10 paragraphs, 50 sentences, 300 words, 2 kB)',
-        function (done) {
-            new Retext().parse(small);
-            done();
-        }
-    );
-
-    bench('medium (100 paragraphs, 500 sentences, 3000 words, 21 kB)',
-        function (done) {
-            new Retext().parse(medium);
-            done();
-        }
-    );
-
-    bench('large (1000 paragraphs, 5000 sentences, 30000 words, 208 kB)',
-        function (done) {
-            new Retext().parse(large);
-            done();
+    bench('An article (100 paragraphs, 500 sentences, 10,000 words)',
+        function () {
+            retext.parse(article);
         }
     );
 });
