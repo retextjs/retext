8574edc5aa733e725c9dcb8621f2e8f7d0b5d5ce
Titus Wormer <tituswormer@gmail.com>
2019-06-05 14:58:51 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -3,73 +3,63 @@
 var test = require('tape')
 var clean = require('unist-util-remove-position')
 var nlcst = require('nlcst-test')
+var u = require('unist-builder')
 var unified = require('./packages/retext/node_modules/unified')
 var retext = require('./packages/retext')
 
-var root = {
-  type: 'RootNode',
-  children: [
-    {
-      type: 'ParagraphNode',
-      children: [
-        {
-          type: 'SentenceNode',
-          children: [
-            {
-              type: 'WordNode',
-              children: [{type: 'TextNode', value: 'Alfred'}]
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
+var parsers = ['latin', 'english', 'dutch']
 
-// Test `retext-latin`, `retext-english`, `retext-dutch`.
-test('retext().parse(file)', function(t) {
-  var parsers = ['latin', 'english', 'dutch']
+test('.parse', function(t) {
+  var tree = retext().parse('Alfred')
 
-  t.test('retext', function(st) {
-    var tree = retext().parse('Alfred')
+  t.doesNotThrow(function() {
+    nlcst(tree)
+  }, 'should parse to valid nlcst')
 
-    st.doesNotThrow(function() {
-      nlcst(tree)
-    }, 'should expose nlcst')
-
-    st.deepEqual(clean(tree, true), root, 'should give the correct tree')
-
-    st.end()
-  })
-
-  parsers.forEach(function(name) {
-    var lib = require('./packages/retext-' + name)
-    var tree = unified()
-      .use(lib)
-      .parse('Alfred')
-
-    t.test('retext-' + name, function(st) {
-      st.doesNotThrow(function() {
-        nlcst(tree)
-      }, 'should expose nlcst')
-
-      st.deepEqual(clean(tree, true), root, 'should give the corrent tree')
-
-      st.end()
-    })
-  })
+  t.deepEqual(
+    clean(tree, true),
+    u('RootNode', [
+      u('ParagraphNode', [
+        u('SentenceNode', [u('WordNode', [u('TextNode', 'Alfred')])])
+      ])
+    ]),
+    'should give the correct tree'
+  )
 
   t.end()
 })
 
-// Test `retext-stringify`.
-test('retext().stringify(ast, file, options?)', function(t) {
+parsers.forEach(function(name) {
+  var tree = unified()
+    .use(require('./packages/retext-' + name))
+    .parse('Alfred')
+
+  test('retext-' + name, function(t) {
+    t.doesNotThrow(function() {
+      nlcst(tree)
+    }, 'should parse to valid nlcst')
+
+    t.deepEqual(
+      clean(tree, true),
+      u('RootNode', [
+        u('ParagraphNode', [
+          u('SentenceNode', [u('WordNode', [u('TextNode', 'Alfred')])])
+        ])
+      ]),
+      'should give the corrent tree'
+    )
+
+    t.end()
+  })
+})
+
+test('.stringify', function(t) {
   t.throws(
     function() {
       retext().stringify(false)
     },
     /false/,
-    'should throw when `ast` is not given'
+    'should throw when `tree` is not given'
   )
 
   t.throws(
@@ -77,7 +67,7 @@ test('retext().stringify(ast, file, options?)', function(t) {
       retext().stringify({})
     },
     /Expected node, got `\[object Object]`/,
-    'should throw when `ast` is not a node'
+    'should throw when `tree` is not a node'
   )
 
   t.throws(
@@ -85,10 +75,20 @@ test('retext().stringify(ast, file, options?)', function(t) {
       retext().stringify({type: 'unicorn'})
     },
     /Cannot read property 'length' of undefined/,
-    'should throw when `ast` is not a valid node'
+    'should throw when `tree` is not a valid node'
   )
 
-  t.deepEqual(retext().stringify(root), 'Alfred', 'should stringify')
+  t.deepEqual(
+    retext().stringify(
+      u('RootNode', [
+        u('ParagraphNode', [
+          u('SentenceNode', [u('WordNode', [u('TextNode', 'Alfred')])])
+        ])
+      ])
+    ),
+    'Alfred',
+    'should stringify'
+  )
 
   t.end()
 })
