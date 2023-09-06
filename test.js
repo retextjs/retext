f586af3d23b5631d9d029eb9013848dd6bfc9b46
Titus Wormer <tituswormer@gmail.com>
2023-09-06 10:48:17 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -4,11 +4,17 @@
 
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import {removePosition} from 'unist-util-remove-position'
 import {emojiModifier} from 'nlcst-emoji-modifier'
 import {assert as nlcstAssert} from 'nlcst-test'
-import {unified} from 'unified'
 import {retext} from 'retext'
+import {unified} from 'unified'
+import {removePosition} from 'unist-util-remove-position'
+
+test('retext', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('retext')).sort(), ['retext'])
+  })
+})
 
 test('parse', async function (t) {
   await t.test('should parse', async function () {
@@ -38,65 +44,6 @@ test('parse', async function (t) {
       ]
     })
   })
-
-  const parsers = ['dutch', 'english', 'latin']
-
-  let index = -1
-  while (++index < parsers.length) {
-    await eachParser(parsers[index])
-  }
-
-  /** @param {string} name */
-  async function eachParser(name) {
-    await t.test('retext-' + name, async function (t) {
-      const fp = './packages/retext-' + name + '/index.js'
-
-      /** @type {{default: import('unified').Plugin<[], string, Root>}} */
-      // type-coverage:ignore-next-line
-      const mod = await import(fp)
-      const processor = unified().use(mod.default)
-
-      await t.test('should parse', async function () {
-        const tree = processor.parse('Alfred')
-
-        nlcstAssert(tree)
-
-        removePosition(tree, {force: true})
-
-        assert.deepEqual(tree, {
-          type: 'RootNode',
-          children: [
-            {
-              type: 'ParagraphNode',
-              children: [
-                {
-                  type: 'SentenceNode',
-                  children: [
-                    {
-                      type: 'WordNode',
-                      children: [{type: 'TextNode', value: 'Alfred'}]
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        })
-      })
-
-      await t.test('should support extensions', async function () {
-        const tree = processor()
-          .data('nlcstSentenceExtensions', [emojiModifier])
-          .parse(':+1:')
-        const paragraph = tree.children[0]
-        assert(paragraph.type === 'ParagraphNode')
-        const sentence = paragraph.children[0]
-        assert(sentence.type === 'SentenceNode')
-        const emoji = sentence.children[0]
-        assert(emoji.type === 'EmoticonNode')
-      })
-    })
-  }
 })
 
 test('stringify', async function (t) {
@@ -125,3 +72,67 @@ test('stringify', async function (t) {
     )
   })
 })
+
+const parsers = ['dutch', 'english', 'latin']
+
+let index = -1
+while (++index < parsers.length) {
+  await eachParser(parsers[index])
+}
+
+/** @param {string} name */
+async function eachParser(name) {
+  await test('retext-' + name, async function (t) {
+    const fp = './packages/retext-' + name + '/index.js'
+
+    /** @type {{default: import('unified').Plugin<[], string, Root>}} */
+    // type-coverage:ignore-next-line
+    const mod = await import(fp)
+
+    await t.test('should expose the public api', async function () {
+      assert.deepEqual(Object.keys(mod).sort(), ['default'])
+    })
+
+    const processor = unified().use(mod.default)
+
+    await t.test('should parse', async function () {
+      const tree = processor.parse('Alfred')
+
+      nlcstAssert(tree)
+
+      removePosition(tree, {force: true})
+
+      assert.deepEqual(tree, {
+        type: 'RootNode',
+        children: [
+          {
+            type: 'ParagraphNode',
+            children: [
+              {
+                type: 'SentenceNode',
+                children: [
+                  {
+                    type: 'WordNode',
+                    children: [{type: 'TextNode', value: 'Alfred'}]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      })
+    })
+
+    await t.test('should support extensions', async function () {
+      const tree = processor()
+        .data('nlcstSentenceExtensions', [emojiModifier])
+        .parse(':+1:')
+      const paragraph = tree.children[0]
+      assert(paragraph.type === 'ParagraphNode')
+      const sentence = paragraph.children[0]
+      assert(sentence.type === 'SentenceNode')
+      const emoji = sentence.children[0]
+      assert(emoji.type === 'EmoticonNode')
+    })
+  })
+}
